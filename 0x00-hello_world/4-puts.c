669ad93e59cacd018f205ad369e58c72a0e6ef92
Spark <fho2841@gmail.com>
2023-06-15 19:12:24 -0700

    w

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - entry point
+ * main - Entry point
  *
  * Description: A C program that prints with put function
  * 
@@ -10,6 +10,6 @@
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
