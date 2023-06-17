c0c1dca557b85f936ea2dc2c29489eec896bb371
Spark <fho2841@gmail.com>
2023-06-16 18:28:24 -0700

    w

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: prints all the numbers of base 16 in lowercase
+ *
+ * Return: Always 0 (succses)
+*/
+
+int main(void)
+{
+	char w = 'a';
+	int m = 0;
+
+	while (m < 16)
+	{
+		if (m < 10)
+			putchar(n + '0');
+		else
+		{
+			putchar(w)
+			w++;
+		}
+		m++;
+	}
+	putchar('\n');
+	return (0);
+}
+
