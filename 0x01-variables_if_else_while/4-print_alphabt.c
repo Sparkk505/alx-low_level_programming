211725c182c99ef9c4bfb08586afd1e8139a5ef6
Spark <fho2841@gmail.com>
2023-06-16 18:01:48 -0700

    w

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,10 +1,9 @@
-
 #include <stdio.h>
 
 /**
  * main - entry point
  *
- * Description: print alphabet in lowercase then in uppercase
+ * Description: Print all the letters except q and e
  *
  * Return: Always 0 (succses)
 */
@@ -12,23 +11,14 @@
 int main(void)
 {
 	char ch = 'a';
-	char CH = 'A';
 
-	/*prints a - z*/
 	while (ch <= 'z')
-        {
+	{
+		if (ch == 'e' || ch == 'q')
+			ch++;
 		putchar(ch);
 		ch++;
 	}
-
-	/*prints A - Z*/
-	while (CH <= 'Z')
-        {
-		putchar(CH);
-		Ch++;
-	}
 	putchar('\n');
- 
 	return (0);
 }
-
