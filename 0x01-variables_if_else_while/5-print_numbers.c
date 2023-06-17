e0d101c345366715cf3fd49f20b899e37b3f969e
Spark <fho2841@gmail.com>
2023-06-16 18:13:36 -0700

    w

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: prints all single digit numbers
+ *
+ * Return: Always 0 (succses)
+*/
+
+int main(void)
+{
+	int n = 0;
+
+	while (n <= 9)
+	{
+		printf("%i", n);
+		n++;
+	}
+	printg('\n');
+	return (0);
+}
