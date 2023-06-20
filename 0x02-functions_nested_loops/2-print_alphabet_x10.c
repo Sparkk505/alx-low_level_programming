031d8cb9019b81e7f204a81351962e461f9ada80
Spark <fho2841@gmail.com>
2023-06-20 13:06:03 -0700

    w

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,11 +5,11 @@
  *  the alphabet a - z 10 times
 */
 
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
 	int ch, i;
 
-	for (i = 0; i <= 10; i++)
+	for (i = 0; i <= 9; i++)
 	{
 		for (ch = 'a'; ch <= 'z'; ch++)
 			_putchar(ch);
