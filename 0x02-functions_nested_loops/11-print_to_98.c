c753c1479d348e1e3469911c85977fc5957d70fc
Spark <fho2841@gmail.com>
2023-06-20 17:44:08 -0700

    w

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,7 +13,7 @@ void  print_to_98(int a)
 
 	if (a <	98)
 	{
-		for (w = a; w <= 98; w++)
+		for (w = a; w < 98; w++)
 		{
 			_putchar(w + '0');
 			_putchar(',');
@@ -22,7 +22,7 @@ void  print_to_98(int a)
 	}
 	else if (a > 98)
 	{
-		for (w = a; w >= 98; w--)
+		for (w = a; w > 98; w--)
 		{
 			_putchar(w + '0');
 			_putchar(',');
