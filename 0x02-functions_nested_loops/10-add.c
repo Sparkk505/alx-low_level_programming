51fa3c723889613bbdee64c86be3f83f215499b4
Spark <fho2841@gmail.com>
2023-06-20 17:27:45 -0700

    w

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include"main.h"
+
+/**
+ * add - add 2 numbers
+ *
+ * @a: first
+ * @b: second
+ *
+ * Return: sum of a & b
+*/
+
+int add(int a, int b)
+{
+	int sum = a + b;
+
+	return (sum);
+}
+
