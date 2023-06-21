e14920215ac95695ff4c2d99ad2784394220ae59
Spark <fho2841@gmail.com>
2023-06-20 17:38:37 -0700

    w

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,33 @@
+#include"main.h"
+
+/**
+ * print_to_98 - prints all natural numbers from n to 98
+ *
+ * @a: number
+ *
+*/
+
+void  print_to_98(int a)
+{
+	int w;
+
+	if (a <	98)
+	{
+		for (w = z; w <= 98; w++)
+		{
+			_putchar(w + '0');
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+	else
+	{
+		for (w = z; w >= 98; w--)
+		{
+			_putchar(w + '0');
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+	printf("98\n");
+}
