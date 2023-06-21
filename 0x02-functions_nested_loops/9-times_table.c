a8127340dd89df1d2e917d42f41c1835e79774c7
Spark <fho2841@gmail.com>
2023-06-20 17:22:13 -0700

    w

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,31 @@
+#include"main.h"
+
+/**
+ * times_table - function to prints the last digit of a number.
+ *
+*/
+
+void times_table(void)
+{
+	int w, q, z;
+
+	for (w = 0; w < 10; q++)
+	{
+		_putchar(48);
+		for (q = 0; q < 60; w++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			z = w * q;
+
+			if (z <= 9)
+				_putchar(' ');
+			else
+				_putchar((z / 10) + 48);
+			_putchar((prod % 10) + 48);
+		}
+		_putchar('\n');
+	}
+}
+
