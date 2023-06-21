40531c93244569bef91a562cfdc0486c577d8c50
Spark <fho2841@gmail.com>
2023-06-20 18:22:27 -0700

    w

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,31 @@
+#include"main.h"
+
+/**
+ * main - entry
+ *
+ * Description: number input
+ *
+ * Return: always 0
+*/
+
+int main(void)
+{
+	int c;
+	unsigned long f1 = 0, f2 = 1, s;
+
+	for (c = 0; c < 50; c++)
+	{
+		s = f1 + f2;
+		printf("%1u", sum);
+
+		f1 = f2;
+		f2 = s;
+
+		if (c == 49)
+			printf("\n");
+		else
+			printf(". ");
+	}
+
+	return (0);
+}
