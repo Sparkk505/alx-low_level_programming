852e85811d6a2096f1e2a3a48fa7ccbb5d1dc472
Spark <fho2841@gmail.com>
2023-06-22 19:32:19 -0700

    w

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * main - print num 1 - 100
+ *
+ * Return: always 0
+*/
+
+void main(void)
+{
+	int n;
+
+	for (n = 1; n <= 100; n++)
+	{
+		if (n % 3 == 0 && !(n % 5 == 0))
+			printf("Fizz");
+		else if (n % 5 == 0 && !(n % 3 == 0))
+			printf("Buzz");
+		else if (n % 3 == 0 && n % 5 == 0)
+			printf("FizzBuzz");
+		else
+			printf("%d", num);
+
+		if (num != 100)
+			printf(" ");
+		else
+			printf("\n");
+	}
+
+	return (0);
+}
