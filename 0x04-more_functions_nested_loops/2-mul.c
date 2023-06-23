2309ddaa5e347255ea35bbf26b93d6925e2910fe
Spark <fho2841@gmail.com>
2023-06-22 18:21:43 -0700

    w

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - print the multiplies of two integers 'a' and 'b'
+ *
+ * @a: the first input
+ * @b: the second input
+ *
+ * Return: a * b
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
