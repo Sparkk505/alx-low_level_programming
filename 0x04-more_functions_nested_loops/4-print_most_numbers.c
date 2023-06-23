0fe89429cbbe3d7fce06249087772c55d238249f
Spark <fho2841@gmail.com>
2023-06-22 18:35:52 -0700

    w

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print nums from 0 to 9
+ *
+ * Return: 0
+*/
+
+void print_most_numbers(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		if (a == 2 || a == 4)
+			break;
+		_putchar(i + 48);
+	}
+	_putchar('\n');
+}
