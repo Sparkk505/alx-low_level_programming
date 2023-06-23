3ee47e575850ccf2acd2bd1810d4317e2dc5659c
Spark <fho2841@gmail.com>
2023-06-22 18:40:29 -0700

    w

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * more_numbers - print nums from 0 to 14, 10 times
+ *
+ * Return: 0
+*/
+
+void more_numbers(void)
+{
+	int a, b;
+
+	for (a = 0; a < 10; a++)
+	{
+		for (b = 0; b < 15; b++)
+		{
+			_putchar(b + 48);
+		}
+	}
+	_putchar('\n');
+}
