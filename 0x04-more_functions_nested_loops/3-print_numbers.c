dc9a5aa70b2aaf81e28eb37aab8fa5528a92d9a6
Spark <fho2841@gmail.com>
2023-06-22 18:27:28 -0700

    w

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - print nums from 0 to 9
+ *
+ * @a: the first input
+ *
+ * Return: a * b
+*/
+
+int print_numbers(int a)
+{
+	for (a = 0; a < 10; a++)
+	{
+		_putchar(i + 48);
+	}
+	_putchar('\n');
+}
