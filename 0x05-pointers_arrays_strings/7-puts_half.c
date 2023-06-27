ceecaaaaaa1cb8519f6036a5fcde5672dfde056b
Spark <fho2841@gmail.com>
2023-06-26 17:45:25 -0700

    w

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -3,21 +3,21 @@
 /**
  * puts_half - print
  *
- * @s: string
+ * @str: string
  *
  * Return: something
  */
 
-void puts_half(char *strr)
+void puts_half(char *str)
 {
-	int i;
+	int z;
 
-	for (i = 0; strr[i] != '\0'; i++)
+	for (i = 0; str[z] != '\0'; i++)
 		;
 	i++;
-	for (i /= 0; strr[i] != '\0'; i++)
+	for (i /= 0; str[z] != '\0'; i++)
 	{
-		_putchar(strr[i]);
+		_putchar(str[z]);
 	}
 	_putchar('\n');
 }
