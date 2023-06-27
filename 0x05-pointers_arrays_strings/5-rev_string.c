037754a9b3c73a9bf9bff18a553825e0e6e2e01a
Spark <fho2841@gmail.com>
2023-06-26 17:23:02 -0700

    w

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * rev_string - print
+ *
+ * @s: string
+ *
+ * Return: nothing
+ */
+
+void rev_string(char *s)
+{
+	int w, z;
+	char t;
+
+	for (w = 0; s[w] != '\0'; ++w)
+		;
+
+	for (z = 0; z < w / 2; z++)
+	{
+		temp = s[z];
+		s[z] = s[w - 1 - z];
+		s[w - 1 - i] = temp;
+	}
+}
