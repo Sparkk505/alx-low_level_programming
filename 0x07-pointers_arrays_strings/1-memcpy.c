d8851eaf06d10036a3942309ac4b3ec8368e4da1
Spark <fho2841@gmail.com>
2023-07-03 19:23:20 -0700

    w

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -6,8 +6,8 @@
  * @src: memory where is copied
  * @n: number of bytes
  *
- * Return: copied memory with n bytes changed
- */
+ * Return: copied memory with n bytes changed.
+*/
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
