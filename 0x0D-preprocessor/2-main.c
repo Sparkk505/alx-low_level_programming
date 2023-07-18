d31e87cfc57655edcdd67d9fb8bb372c088394d8
Spark <fho2841@gmail.com>
2023-07-17 17:30:54 -0700

    w

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -2,21 +2,14 @@
 
 
 /**
-
  * main - prints the name of the file
-
  *
-
  * Return: Always 0 (Success)
-
- */
+*/
 
 int main(void)
-
 {
+	printf("%s\n", __FILE__);
 
-        printf("%s\n", __FILE__);
-
-        return (0);
-
+	return (0);
 }
